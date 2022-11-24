f341a5f192ad7541ebd7761ff76cf148dc4fa158
Eric Lau <ericglau@outlook.com>
2022-11-24 17:34:34 -0500

    Set watch_mode

--- scripts/run.py
+++ scripts/run.py
@@ -3,4 +3,4 @@ from nile.core.account import Account
 
 async def run(nre):
     signer = "PRIVATE_KEY"
-    await Account(signer, nre.network)
+    await Account(signer, nre.network, watch_mode="track")
